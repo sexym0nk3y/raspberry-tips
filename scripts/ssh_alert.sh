5230fe199909074130b1fd3b1bd5648041fdd92c
Chris Rasser <christophe.rasser@gmail.com>
2020-11-27 18:57:13 +0100

    Change SSH login alert to avoid SPAM

--- scripts/ssh_alert.sh
+++ scripts/ssh_alert.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
 RECIPIENT=youremail@email.com
-SUBJECT="Email from your Server: SSH Alert"
+SUBJECT="Email from your Server: Login SSH Alert"
+SENDERMAIL=youremail@email.com
 
 
 BODY="
+From:${SENDERMAIL}
+To:${SENDERMAIL}
+
+Hello,
+
 A SSH login was successful, so here are some information for security:
-  	User:        $PAM_USER
-	User IP Host: $PAM_RHOST
-	Service:     $PAM_SERVICE
-	TTY:         $PAM_TTY
-	Date:        `date`
-	Server:      `uname -a`
+
+User:        $PAM_USER
+User IP Host: $PAM_RHOST
+Service:     $PAM_SERVICE
+TTY:         $PAM_TTY
+Date:        `date`
+Server:      `uname -a`
 "
 
 if [ ${PAM_TYPE} = "open_session" ]; then
